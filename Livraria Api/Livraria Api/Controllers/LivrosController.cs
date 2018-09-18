8762e0118dc123ba182bb64f8c8366aac1421352
Lucas Ramos <manobray@hotmail.com>
2018-09-18 20:38:27 -0300

    Swagger e controller de livro

---
+++ Livraria Api/Livraria Api/Controllers/LivrosController.cs
@@ -0,0 +1,40 @@
+﻿using Livraria_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Livraria_Api.Controllers
+{
+    public class LivrosController : ApiController
+    {
+        // GET: api/Livros
+        public IEnumerable<Livro> Get()
+        {
+            return new Livro[] { new Livro(), new Livro() };
+        }
+
+        // GET: api/Livros/5
+        public Livro Get(int id)
+        {
+            return new Livro();
+        }
+
+        // POST: api/Livros
+        public void Post([FromBody]Livro livro)
+        {
+        }
+
+        // PUT: api/Livros/5
+        public void Put(int id, [FromBody]Livro livro)
+        {
+        }
+
+        // DELETE: api/Livros/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Livraria Api/Livraria Api/Models/Livro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Livraria_Api.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+    }
+}
